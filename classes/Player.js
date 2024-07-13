e27bd73b9238a35d0a04dc2e804fd453555cc86e
Christian Hanne <support@aureola.codes>
2024-07-13 15:31:41 +0200

    Update player data model

--- classes/Player.js
+++ classes/Player.js
@@ -2,5 +2,21 @@ module.exports = class Player {
     constructor(id, name) {
         this.id = id;
         this.name = name;
+        this.isReady = false;
+    }
+
+    getMinResponse() {
+        return {
+            id: this.id,
+            name: this.name
+        };
+    }
+
+    getFullResponse() {
+        return {
+            id: this.id,
+            name: this.name,
+            isReady: this.isReady
+        };
     }
 }

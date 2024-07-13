6c83019a8cedc7078f33204ab4fe0e85397d837d
Christian Hanne <support@aureola.codes>
2024-07-13 10:38:01 +0200

    Remove admin ui for now

--- config.js
+++ config.js
@@ -2,16 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    // Server
     serverPort: process.env.SERVER_PORT || 9000,
     authToken: process.env.AUTH_TOKEN || '',
     maxPlayers: process.env.MAX_PLAYERS || 100,
     maxPlayersPerMatch: process.env.MAX_PLAYERS_PER_MATCH || 4,
-
-    // Admin UI
-    adminEnabled: process.env.ADMINUI_ENABLED || false,
-    adminMode: process.env.ADMINUI_MODE || 'development',
-    adminPort: process.env.ADMINUI_PORT || 3000,
-    adminUsername: process.env.ADMINUI_USERNAME || 'admin',
-    adminPassword: process.env.ADMINUI_PASSWORD || 'admin',
 };
